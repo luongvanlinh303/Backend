83062210fbd869b7b716ddddd66d036552549b74
FPT-DungVC1 <dungvc1@fsoft.com>
2023-12-11 20:06:00 +0700

    git asa

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 // routes/authRoutes.js
 const express = require('express');
 const authenticate = require('../middlewares/authenticate');
-const authController = require('../controllers/authController');
+const authController = require('git controllers/authController');
 
 const router = express.Router();
 /**
